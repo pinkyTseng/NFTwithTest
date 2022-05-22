51b4ada2738ee387ccbcde3d1ebaa25cd6a69778
pinkyTseng <charlie761216@msn.com>
2022-05-22 17:35:36 +0800

    version 1

--- test/sample-test.js
+++ test/sample-test.js
@@ -9,18 +9,6 @@ const nftSymbol = "CFT"
 
 const provider = waffle.provider;
 
-// const { accounts, contract } = require('@openzeppelin/test-environment');
-// const [ owner ] = accounts;
-
-// const CharlieNft = contract.fromArtifact('CharlieNft'); // Loads a compiled contract
-
-// describe('Openzeppelin', function () {
-//   it('Openzeppelin test', async function () {
-//     const charlieNft = await CharlieNft.new({ from: owner });
-//     expect(await charlieNft.owner()).to.equal(owner);
-//   });
-// });
-
 describe("CharlieNft", function () {
   let charlieNft;
   let owner, user1, user2, users;
@@ -55,14 +43,22 @@ describe("CharlieNft", function () {
       // result.should.be.bignumber.equal(ethers.utils.parseEther('0.01'));
     });
 
-    // it("other mint 1 nft", async function () {      
-    //   await greeter.connect(user1)
+    it("other mint 1 nft", async function () {
+      let balanceInWeiOld = await provider.getBalance(charlieNft.address);      
+      await charlieNft.openSell();
+      
+      let user1Connrct = await charlieNft.connect(user1);
+      await user1Connrct.mint(1, {value: ethers.utils.parseEther('0.01')} );
 
-    //   await charlieNft.openSell();
-    //   await charlieNft.mint(1, {value: ethers.utils.parseEther('0.01')} );
-    //   let theTOkenOwner = await charlieNft.ownerOf(1);
-    //   expect(theTOkenOwner).to.be.equal(user1.address); 
-    // });
+      let theTOkenOwner = await charlieNft.ownerOf(1);
+      expect(theTOkenOwner).to.be.equal(user1.address);
+
+      expect(1).to.equal(await charlieNft.totalSupply());
+
+      let balanceInWei = await provider.getBalance(charlieNft.address);
+      let result = balanceInWei.sub(balanceInWeiOld)
+      expect(result).to.equal(ethers.utils.parseEther('0.01'));
+    });
 
     it("tokenURI test", async function () {      
       await charlieNft.openSell();
@@ -89,34 +85,14 @@ describe("CharlieNft", function () {
 
     it("withdraw test", async function () {      
       await charlieNft.openSell();
-      await charlieNft.mint(1, {value: ethers.utils.parseEther('0.01')} );
+      let ownerBalanceOld = await owner.getBalance()
+      await charlieNft.connect(user1).mint(2, {value: ethers.utils.parseEther('0.02')} );
       await charlieNft.withdraw();
-      expect(1).to.equal(1);
+      let ownerBalance = await owner.getBalance()
+      assert(ownerBalance.gt(ownerBalanceOld), 'owner not get money from contract');
     });
 
 
   });
 });
 
-// describe("CharlieNft2", function () {
-//   let charlieNft;
-//   let owner, user1, user2, users;
-
-//   beforeEach(async () => {
-//     console.log("beforeEach2 run");
-//     [owner, user1, user2, ...users] = await ethers.getSigners();
-//     const CharlieNft = await ethers.getContractFactory("CharlieNft");
-//     charlieNft = await CharlieNft.deploy(nftName, nftSymbol);
-//     await charlieNft.deployed();
-//   });
-
-//   describe("happy path flow", async function () {
-//     it("happy path flow sell and mint nft success", async function () {
-//       charlieNft.setUnrevealedUrl(UnrevealedUrl);
-//       charlieNft.setRevealedUrl(RevealedUrl);
-//       charlieNft.openSell();
-//       charlieNft.mint(1, {value: ethers.utils.parseEther('0.01')} );
-//       expect(1).to.be.equal(1);
-//     });
-//   });
-// });
