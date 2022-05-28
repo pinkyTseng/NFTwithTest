fd0ff80734b42931dd101d4b4ae36877b659f294
pinkyTseng <charlie761216@msn.com>
2022-05-28 20:59:13 +0800

    add exception path test case

--- test/sample-test.js
+++ test/sample-test.js
@@ -34,8 +34,11 @@ async function getBeforeMintData(contractInstance, caller){
 async function checkAfterMintData(beforData, mintCount, contractInstance, caller){
   return new Promise(async function(resolve, reject){
     try{      
-      expect(beforData.callerOwnCount + mintCount).to.equal(await contractInstance.balanceOf(caller.address));
-      expect(beforData.totalCount+ mintCount).to.equal(await contractInstance.totalSupply());
+      // expect(beforData.callerOwnCount + mintCount).to.equal(await contractInstance.balanceOf(caller.address));
+      // expect(beforData.totalCount+ mintCount).to.equal(await contractInstance.totalSupply());
+
+      expect(parseInt(beforData.callerOwnCount) + parseInt(mintCount)).to.equal(await contractInstance.balanceOf(caller.address));
+      expect(parseInt(beforData.totalCount)+ parseInt(mintCount)).to.equal(await contractInstance.totalSupply());
           
       let balanceInWeiNew = await provider.getBalance(contractInstance.address);
       addedBalanceInWei = balanceInWeiNew.sub(beforData.balanceInWei);
@@ -56,7 +59,7 @@ describe("CharlieNft", function () {
   describe("happy path flow", async function () {
 
     beforeEach(async () => {
-      console.log("beforeEach run");
+      // console.log("beforeEach run");
       [owner, user1, user2, ...users] = await ethers.getSigners();
       const CharlieNft = await ethers.getContractFactory("CharlieNft");
       charlieNft = await CharlieNft.deploy(nftName, nftSymbol);
@@ -83,25 +86,17 @@ describe("CharlieNft", function () {
       let val = 0.01 * mintCount;
       let user1Connrct = await charlieNft.connect(user1);
       await user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString() )});
-      // await expect(user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString())} )) .to.be.reverted;      
       await checkAfterMintData(preMinedData, mintCount, charlieNft, user1);
-    });    
-
-    it("other mint 3 nft", async function () {         
-      // await charlieNft.openSell();
-      let mintCount = 3;
+    });
+    
+    it("other mint 2 nft", async function () {
+      let mintCount = 2;
       let preMinedData = await getBeforeMintData(charlieNft, user1);
       let val = 0.01 * mintCount;
       let user1Connrct = await charlieNft.connect(user1);
-      // await user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString() )});
-      await expect(user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString())} )) .to.be.reverted;      
-      // await expect(user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString() )})).to.be.reverted;
-      await checkAfterMintData(preMinedData, 0, charlieNft, user1);
-    });
-
-
-
-
+      await user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString() )});
+      await checkAfterMintData(preMinedData, mintCount, charlieNft, user1);
+    }); 
 
     it("tokenURI test", async function () {      
       // await charlieNft.openSell();
@@ -134,8 +129,107 @@ describe("CharlieNft", function () {
       let ownerBalance = await owner.getBalance()
       assert(ownerBalance.gt(ownerBalanceOld), 'owner not get money from contract');
     });
+  });
 
+
+
+  describe("mint excepion path flow fail right", async function () {
+    beforeEach(async () => {
+      // console.log("beforeEach run");
+      [owner, user1, user2, ...users] = await ethers.getSigners();
+      const CharlieNft = await ethers.getContractFactory("CharlieNft");
+      charlieNft = await CharlieNft.deploy(nftName, nftSymbol);
+      await charlieNft.deployed();
+      await charlieNft.setUnrevealedUrl(UnrevealedUrl);
+      await charlieNft.setRevealedUrl(RevealedUrl);
+
+      await charlieNft.openSell();      
+    });
+
+    it("noActive can't mint", async function () {
+      await charlieNft.pauseSell();
+      let mintCount = 1;
+      let preMinedData = await getBeforeMintData(charlieNft, user1);
+      let val = 0.01 * mintCount;
+      let user1Connrct = await charlieNft.connect(user1);
+      await expect(user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString())} )) .to.be.reverted;
+      await checkAfterMintData(preMinedData, 0, charlieNft, user1);      
+    });
+
+    it("not so many nfts", async function () {
+      let mintCount = 20;
+      let preMinedData = await getBeforeMintData(charlieNft, user1);
+      let val = 0.01 * mintCount;
+      let user1Connrct = await charlieNft.connect(user1);
+      await expect(user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString())} )) .to.be.reverted;
+      await checkAfterMintData(preMinedData, 0, charlieNft, user1);      
+    });
+
+    it("over eachMintMaxCount", async function () {         
+      // await charlieNft.openSell();
+      let mintCount = 3;
+      let preMinedData = await getBeforeMintData(charlieNft, user1);
+      let val = 0.01 * mintCount;
+      let user1Connrct = await charlieNft.connect(user1);
+      await expect(user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString())} )) .to.be.reverted;      
+      await checkAfterMintData(preMinedData, 0, charlieNft, user1);
+    });
+
+    it("over individual maxOwn", async function () {
+      let mintCount = 2;
+      let preMinedData = await getBeforeMintData(charlieNft, user1);
+      let val = 0.01 * mintCount;
+      let user1Connrct = await charlieNft.connect(user1);
+      await user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString() )});
+      await checkAfterMintData(preMinedData, mintCount, charlieNft, user1);
+
+      mintCount = 2;
+      preMinedData = await getBeforeMintData(charlieNft, user1);
+      val = 0.01 * mintCount;
+      await user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString() )});
+      await checkAfterMintData(preMinedData, mintCount, charlieNft, user1);
+
+      mintCount = 2;
+      preMinedData = await getBeforeMintData(charlieNft, user1);
+      val = 0.01 * mintCount;
+      await expect(user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( val.toString())} )) .to.be.reverted;
+      await checkAfterMintData(preMinedData, 0, charlieNft, user1);
+      
+    });
+
+
+    it("paid for price lower than needed", async function () {
+      let mintCount = 2;
+      let preMinedData = await getBeforeMintData(charlieNft, user1);
+      let user1Connrct = await charlieNft.connect(user1);
+      await expect(user1Connrct.mint(mintCount, {value: ethers.utils.parseEther( "0.01" )} )) .to.be.reverted;
+      await checkAfterMintData(preMinedData, 0, charlieNft, user1);       
+    });    
 
   });
+
+  describe("other less important excepion fail right", async function () {
+    beforeEach(async () => {
+      // console.log("beforeEach run");
+      [owner, user1, user2, ...users] = await ethers.getSigners();
+      const CharlieNft = await ethers.getContractFactory("CharlieNft");
+      charlieNft = await CharlieNft.deploy(nftName, nftSymbol);
+      await charlieNft.deployed();        
+    });
+
+    it("setUnrevealedUrl empty", async function () {
+      await expect(charlieNft.setUnrevealedUrl("")) .to.be.reverted;      
+    });
+
+    it("setRevealedUrl empty", async function () {
+      await expect(charlieNft.setRevealedUrl("")) .to.be.reverted;      
+    });
+
+    it("ERC721Metadata: URI query for nonexistent token", async function () {
+      await expect(charlieNft.tokenURI(1)) .to.be.reverted;      
+    });
+  });
+
+
 });
 
